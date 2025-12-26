2fdf650d8986a973fe45072aaa3c7f48f3ecd1cb
seraf7 <zaske_4@hotmail.com>
2025-12-26 13:36:11 -0600

    Nuevos paquetes y formateo de codigo

--- test_wikibot.py
+++ test_wikibot.py
@@ -2,12 +2,13 @@ from mylib.bot import scrape
 from wikibot import cli
 from click.testing import CliRunner
 
+
 def test_scrape():
     assert "Sucker Punch" in scrape("Sucker Punch film")
 
+
 def test_wikibot():
     runner = CliRunner()
-    resultado = runner.invoke(
-        cli, ["--term", "Sucker Punch film", "--orac", 3])
+    resultado = runner.invoke(cli, ["--term", "Sucker Punch film", "--orac", 3])
     assert resultado.exit_code == 0
     assert "Sucker Punch" in resultado.output
--- wikibot.py
+++ wikibot.py
@@ -1,12 +1,14 @@
 import click
 from mylib.bot import scrape
 
+
 @click.command()
 @click.option("--term", help="Termino que se quiere buscar")
 @click.option("--orac", help="Cantidad de oraciones en el resultado")
 def cli(term, orac):
     resultado = scrape(term, orac)
     click.echo(click.style(f"{resultado}:", fg="cyan"))
+
 
 if __name__ == "__main__":
     cli()
